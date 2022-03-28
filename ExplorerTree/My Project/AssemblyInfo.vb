befa4f253c9227df0da466edb664af285165a94f
Johannes Hanek <73287080+johanneshanek@users.noreply.github.com>
2022-03-28 13:35:25 +0200

    Add project files.

---
+++ ExplorerTree/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ExplorerTree")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Fraunhofer ISE")>
+<Assembly: AssemblyProduct("ExplorerTree")>
+<Assembly: AssemblyCopyright("Copyright © Fraunhofer ISE 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7455a2cc-245a-42e1-bbe7-36ac8fa2deba")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
